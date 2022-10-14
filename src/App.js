076a033556b374d5475a8ccb308bbd90c5517253
GuilhermeNotFound404 <leonadokof132@gmail.com>
2022-10-14 18:01:21 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import tmdb from "./tmdb";
-import './App.css';
+import "./App.css";
 import MovieRow from "./components/MovieRow";
+import FeatueMovie from "./components/FeatueMovie";
 
+const App = () => {
+  const [movieList, setMovieList] = useState([]);
+  const [FeaturedData, setFeaturedData] = useState(null);
 
-export default () =>{
-
-  const [movieList, setMovieList] = useState ([]);
-
-
-useEffect(()=>{
   const loadAll = async () => {
     let list = await tmdb.getHomeList();
+
+    let originals = list.filter((i) => i.slug === "originals");
+    let randomChosen = Math.floor(
+      Math.random() * (originals[0].items.results.length - 1)
+    );
+    let chosen = originals[0].items.results[randomChosen];
+
+    console.log(chosen);
+
     setMovieList(list);
-  }
+  };
 
-  loadAll();
-}, []);
-
-
+  useEffect(() => {
+    loadAll();
+  }, []);
 
   return (
     <div className="page">
+      {setFeaturedData && <FeatueMovie item={setFeaturedData} />}
+
       <section className="lists">
-        {movieList.map((item, key)=>(
-         <MovieRow key={key} title={item.title} items={item.items}/>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div>
   );
-}
+};
+
+export default memo(App);
---
+++ src/components/FeatueMovie.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./FeaturedMovie.css";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return <section className="featured">filme</section>;
+};
